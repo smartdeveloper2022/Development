d1e3421fc3c510f4bf35f2e305af74950a3b3193
smartdeveloper2021 <83185336+smartdeveloper2021@users.noreply.github.com>
2021-05-28 02:16:15 +0530

    First Commit into Development Branch

---
+++ gg/src/main/java/com/test/maven/gg/App.java
@@ -0,0 +1,13 @@
+package com.test.maven.gg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
